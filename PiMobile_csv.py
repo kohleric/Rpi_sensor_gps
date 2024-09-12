4e7202341e0b48c4ce63b0085780a7d4f8690508
kohleric <62691373+kohleric@users.noreply.github.com>
2024-09-12 10:32:33 +0200

    suppression des print pour tâche de fond

--- PiMobile_csv.py
+++ PiMobile_csv.py
@@ -22,23 +22,23 @@ data_dir = '../data_PiMobile'
 today_dir = os.path.join(data_dir, today)
 
 if not os.path.exists(data_dir):
-    print(f"Le répertoire '{data_dir}' n'existe pas. Création...")
+    #print(f"Le répertoire '{data_dir}' n'existe pas. Création...")
     os.makedirs(data_dir)
 else:
-    print(f"Le répertoire '{data_dir}' existe déjà.")
+    #print(f"Le répertoire '{data_dir}' existe déjà.")
 
 if not os.path.exists(today_dir):
-    print(f"Le répertoire '{today_dir}' n'existe pas. Création...")
+    #print(f"Le répertoire '{today_dir}' n'existe pas. Création...")
     os.makedirs(today_dir)
 else:
-    print(f"Le répertoire '{today_dir}' existe déjà.")
+    #print(f"Le répertoire '{today_dir}' existe déjà.")
 
 
 csv_file = os.path.join(today_dir, today+".csv")
 
 sgp30 = SGP30()
 
-print("SGP30 warming up, please wait...Press Ctrl+C to exit!")
+#print("SGP30 warming up, please wait...Press Ctrl+C to exit!")
 def crude_progress_bar():
     sys.stdout.write('.')
     sys.stdout.flush()
@@ -56,11 +56,11 @@ while True:
     timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
     #plt.text(0.1, 0.7, time.time())#•, horizontalalignment='center',verticalalignment='center')
     result = sgp30.get_air_quality()
-    print(result)
+    #print(result)
     #plt.text(0.1, 0.6, result)
     temperature = bmp280.get_temperature()
     pressure = bmp280.get_pressure()
-    print(f"{temperature:05.2f}*C {pressure:05.2f}hPa")
+    #print(f"{temperature:05.2f}*C {pressure:05.2f}hPa")
     #plt.text(0.1, 0.5, f"{temperature:05.2f}*C {pressure:05.2f}hPa")#•, horizontalalignment='center',verticalalignment='center')
     bus.write_i2c_block_data(0x38, 0xE1, config)
     time.sleep(0.5)
@@ -73,20 +73,20 @@ while True:
     #print(data)
     temp = ((data[3] & 0x0F) << 16) | (data[4] << 8) | data[5]
     ctemp = ((temp*200) / 1048576) - 50
-    print(u'Temperature: {0:.1f}°C'.format(ctemp))
+    #print(u'Temperature: {0:.1f}°C'.format(ctemp))
     #plt.text(0.1, 0.4, u'Temperature: {0:.1f}°C'.format(ctemp))#•, horizontalalignment='center',verticalalignment='center')
     tmp = ((data[1] << 16) | (data[2] << 8) | data[3]) >> 4
     #print(tmp)
     ctmp = int(tmp * 100 / 1048576)
     #plt.text(0.1, 0.4, u'Humidity: {0}%'.format(ctmp))
-    print(u'Humidity: {0}%'.format(ctmp))
+    #print(u'Humidity: {0}%'.format(ctmp))
     
     with open(csv_file, mode='a', newline='') as file:
         writer = csv.writer(file)
         
         writer.writerow([timestamp, result])
     
-    print(f"Données enregistrées : {timestamp}, {result}")
+    #print(f"Données enregistrées : {timestamp}, {result}")
     
     time.sleep(1.0)
     
